8abb523511cc2e0fbea8bf6f9cba92395780492d
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-09-07 17:57:26 +0200

    mm

---
+++ 3pr_gr2/cw1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello, World! i nowa linia");
+
+Console.Write("Hello, World! I BEZ NOWEJ LINII" + Environment.NewLine);
+Console.Write("Hello, World! I BEZ NOWEJ LINII\n");
+Console.WriteLine("Ala ma kota".ToUpper());
+
+
