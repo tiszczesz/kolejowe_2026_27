af68199002444d760da46c771e3cba42fda44ae8
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-09-14 17:30:20 +0200

    kk

--- 3pr_gr2/cw2/Program.cs
+++ 3pr_gr2/cw2/Program.cs
@@ -3,7 +3,7 @@
 //ćwiczenie:
 // Napisz funkcję UserRegister(), która prosi o imię i nazwisko oraz wiek
 // I rejestruje na egzamin prawa jazdy jeżeli wiek jest większy lub równy 17 lat, 
-// w przeciwnym wypadku informuje, że kandydat jest niepełnoletni
+// w przeciwnym wypadku informuje, że kandydat jest za młody
 void FirstElem()
 {
     //tworzenie nowej zmiennej
