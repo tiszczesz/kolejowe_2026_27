f86c1d3dad5c9c929a634d6cd163d2fb04739466
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-09-14 18:58:38 +0200

    kk

--- 3pr_gr1/cw1/Program.cs
+++ 3pr_gr1/cw1/Program.cs
@@ -1,4 +1,33 @@
-﻿Console.Write("Hello, World!\n");
-Console.Write("Hello, World!" + Environment.NewLine);
-Console.WriteLine("Hello, World!");
+﻿//Ex1();
+Ex2();
+
+
+void Ex1()
+{
+    Console.Write("Hello, World!\n");
+    Console.Write("Hello, World!" + Environment.NewLine);
+    Console.WriteLine("Hello, World!");
+}
+void Ex2()
+{
+    //operacje wejścia/wyjścia
+    Console.Write("Podaj a: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Podaj b: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{a} + {b} = {a + b}");
+    Console.WriteLine($"{a} - {b} = {a - b}");
+    Console.WriteLine($"{a} * {b} = {a * b}");
+    if (b != 0)
+    {
+        Console.WriteLine($"{a} / {b} = {(double)a / b}");
+    }
+    else
+    {
+        Console.WriteLine("Nie można dzielić przez 0");
+    }
+    Console.WriteLine(b != 0 ? $"{a} / {b} = {(double)a / b}"
+                                 : "Nie można dzielić przez 0");
+    //Console.WriteLine(a + " + " + b + " = " + (a + b));
+}
 
