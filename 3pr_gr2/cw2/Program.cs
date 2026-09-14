66dd3e8f79252012ddb39d1e7dbba7e960764c24
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-09-14 17:29:42 +0200

    kk

--- 3pr_gr2/cw2/Program.cs
+++ 3pr_gr2/cw2/Program.cs
@@ -1,6 +1,9 @@
 ﻿//FirstElem();
-SecondElem();
-
+//SecondElem();
+//ćwiczenie:
+// Napisz funkcję, która prosi o imię i nazwisko oraz wiek
+// I rejestruje na egzamin prawa jazdy jeżeli wiek jest większy lub równy 17 lat, 
+// w przeciwnym wypadku informuje, że kandydat jest niepełnoletni
 void FirstElem()
 {
     //tworzenie nowej zmiennej
