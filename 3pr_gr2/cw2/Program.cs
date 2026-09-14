99f93b54edab2c1a64f69c257ab94c571e568808
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-09-14 17:20:53 +0200

    kk

--- 3pr_gr2/cw2/Program.cs
+++ 3pr_gr2/cw2/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿FirstElem();
+SecondElem();
+
+void FirstElem()
+{
+    //tworzenie nowej zmiennej
+    // zmienna liczba całkowita
+    int number = 10; //typ nazwa = wartosc
+    Console.WriteLine($" a = {number}");
+    //a = "ala ma kota"; // błąd kompilacji, nie można przypisać string do int
+    string text = "ala ma kota"; //inna zmienna typu string
+    Console.WriteLine($"tekst = {text}\t liczba = {number}"); // \t - tabulator
+}
+
+void SecondElem()
+{
+    Console.Write("Podaj imie: ");
+    string? firstName = Console.ReadLine();// wczytanie danych z konsoli
+    Console.Write("Podaj wiek: ");
+    int age = Convert.ToInt32(Console.ReadLine());
+    if (age < 18) //instrukcja warunkowa
+    {
+        Console.WriteLine($"Witaj {firstName} wiek: {age}, jesteś niepełnoletni");
+    }
+    else
+    {
+        Console.WriteLine($"Witaj {firstName} wiek: {age}, jesteś pełnoletni");
+    }
+}
