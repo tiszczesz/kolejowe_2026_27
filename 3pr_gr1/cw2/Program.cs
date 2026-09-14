4241a9f630f8f672ea511a91532a51b80a809f12
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-09-14 19:24:44 +0200

    kk

---
+++ 3pr_gr1/cw2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Ex1();
+Ex2(200);//100
+void Ex1()
+{
+    Console.WriteLine("podaj ilosc liczb: ");
+    int n = int.Parse(Console.ReadLine() ?? "10");
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"{i}\t{Math.Pow(i, 2)}\t{Math.Pow(i, 3)}");
+    }
+}
+void Ex2(uint range = 100)
+{
+    //while(){...}
+    int sum = 0;
+    Random rnd = new Random(); //losowacz
+    while(sum < range)
+    {
+        int randomNumber = rnd.Next(20);
+        sum += randomNumber;
+        Console.Write(randomNumber + " ");
+    }
+    Console.WriteLine($"\nSuma: {sum}");
+}
