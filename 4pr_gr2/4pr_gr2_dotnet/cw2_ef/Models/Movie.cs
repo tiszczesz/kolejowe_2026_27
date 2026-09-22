d8259c959d389578b51729f854c1ee9492b1c9d9
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-09-22 09:37:07 +0200

    kk

--- 4pr_gr2/4pr_gr2_dotnet/cw2_ef/Models/Movie.cs
+++ 4pr_gr2/4pr_gr2_dotnet/cw2_ef/Models/Movie.cs
@@ -5,8 +5,8 @@ namespace cw2_ef.Models;
 public class Movie
 {
     public int Id { get; set; }
-    public string Title { get; set; } = null!;
-    public string Director { get; set; } = null!;
+    public string? Title { get; set; } 
+    public string? Director { get; set; } 
     public DateTime ReleaseDate { get; set; }
 
 }
